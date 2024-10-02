7d286e370890cd5ab631a3223cf897a4d9f9da1b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-10-02 11:33:17 +0100

    PreviuousBase

---
+++ app/src/main/java/com/eliezerantonio/corroutinasfundamentals/PreviousBases.kt
@@ -0,0 +1,50 @@
+package com.eliezerantonio.corroutinasfundamentals
+
+import kotlin.concurrent.thread
+import kotlin.random.Random
+
+fun main() {
+
+    lambda()
+    threads()
+}
+
+fun threads() {
+    println(multiThread(2, 3))
+}
+
+
+fun multiThread(x: Int, y: Int): Int {
+    var result = 0
+
+    //background task
+    thread {
+        Thread.sleep(someTime())
+        result = x * y
+    }
+    return result
+
+}
+
+fun someTime(): Long = Random.nextLong(500, 2_000)
+
+
+fun lambda() {
+    println(multi(2, 3))
+
+    multiLambda(3, 2) { result ->
+        println(result)
+    }
+
+}
+
+fun multiLambda(x: Int, y: Int, callback: (result: Int) -> Unit) {
+    callback(x * y)
+
+}
+
+fun multi(x: Int, y: Int): Int {
+
+    return x * y
+
+}
