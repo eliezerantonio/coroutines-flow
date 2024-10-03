beb4c8ee5c97a68df9a8284e9a5091f353e37966
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-10-03 19:54:57 +0100

    suspend

--- app/src/main/java/com/eliezerantonio/corroutinasfundamentals/CoroutinesBasic.kt
+++ app/src/main/java/com/eliezerantonio/corroutinasfundamentals/CoroutinesBasic.kt
@@ -1,13 +1,24 @@
 package com.eliezerantonio.corroutinasfundamentals
 
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-fun main(){
-    globalScope()
+fun main() {
+    //  globalScope()
+    suspendFun()
 
     readln()
+}
+
+@OptIn(DelicateCoroutinesApi::class)
+fun suspendFun() {
+    newTopic("Suspended Function")
+    Thread.sleep(someTime())
+    GlobalScope.launch {
+        delay(someTime())
+    }
 }
 
 fun globalScope() {
