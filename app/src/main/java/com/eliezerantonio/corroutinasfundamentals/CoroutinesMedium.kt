5924810e236193dc024c860e560c2507edebab0f
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-10-08 23:30:57 +0100

    sequencies, Withcontext and flow

--- app/src/main/java/com/eliezerantonio/corroutinasfundamentals/CoroutinesMedium.kt
+++ app/src/main/java/com/eliezerantonio/corroutinasfundamentals/CoroutinesMedium.kt
@@ -1,17 +1,114 @@
 package com.eliezerantonio.corroutinasfundamentals
 
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.random.Random
 
-
-fun main() {
-    dispatchers()
+fun main(){
+    //dispatchers()
+    //nested()
+    //changeWithContext()
+    basicFlows()
 }
 
-@OptIn(DelicateCoroutinesApi::class)
+fun basicFlows() {
+    newTopic("Flows básicos")
+    runBlocking {
+        launch { getDataByFlow().collect { println(it) } }
+
+        launch {
+            (1..50).forEach {
+                delay(someTime()/10)
+                println("Tarea 2...")
+            }
+        }
+    }
+}
+fun getDataByFlow(): Flow<Float> {
+    return flow {
+        (1..5).forEach {
+            println("procesando datos...")
+            delay(someTime())
+            emit(20 + it + Random.nextFloat())
+        }
+    }
+}
+
+fun changeWithContext() {
+    runBlocking {
+        newTopic("withContext")
+        startMsg()
+
+        withContext(newSingleThreadContext("Cursos Android ANT")){
+            startMsg()
+            delay(someTime())
+            println("CursosAndroidANT")
+            endMsg()
+        }
+
+        withContext(Dispatchers.IO){
+            startMsg()
+            delay(someTime())
+            println("Petición al servidor")
+            endMsg()
+        }
+
+        endMsg()
+    }
+}
+
+fun nested() {
+    runBlocking {
+        newTopic("Anidar")
+
+        val job = launch {
+            startMsg()
+
+            launch {
+                startMsg()
+                delay(someTime())
+                println("Otra tarea")
+                endMsg()
+            }
+
+            val subJob = launch(Dispatchers.IO) {
+                startMsg()
+
+                launch(newSingleThreadContext("Cursos Android ANT")) {
+                    startMsg()
+                    println("tarea cursos android ant")
+                    endMsg()
+                }
+
+                delay(someTime())
+                println("tarea en el servidor")
+                endMsg()
+            }
+            delay(someTime()/4)
+            subJob.cancel()
+            println("SubJob cancelado...")
+
+            var sum = 0
+            (1..100).forEach {
+                sum += it
+                delay(someTime()/100)
+            }
+            println("Sum = $sum")
+            endMsg()
+        }
+
+        delay(someTime()/2)
+        job.cancel()
+        println("Job cancelado...")
+    }
+}
+
 fun dispatchers() {
     runBlocking {
         newTopic("Dispatchers")
@@ -20,39 +117,32 @@ fun dispatchers() {
             println("None")
             endMsg()
         }
-
         launch(Dispatchers.IO) {
             startMsg()
-            println("None")
+            println("IO")
             endMsg()
         }
-
         launch(Dispatchers.Unconfined) {
             startMsg()
             println("Unconfined")
             endMsg()
         }
-
-        // for android main and default
-
+        //Main solo para android
         launch(Dispatchers.Default) {
             startMsg()
             println("Default")
             endMsg()
         }
-
-        launch(newSingleThreadContext("Android Course ")) {
+        launch(newSingleThreadContext("Cursos Android ANT")) {
             startMsg()
-            println("My custom Coroutine with dispatcher")
+            println("Mi corrutina personalizada con un dispatcher")
             endMsg()
-
         }
-        newSingleThreadContext(" CurdoAndroidANT").use { myContext ->
+        newSingleThreadContext("CursosAndroidANT").use { myContext ->
             launch(myContext) {
                 startMsg()
-                println("My custom Coroutine con un dispatcher2")
+                println("Mi corrutina personalizada con un dispatcher2")
                 endMsg()
-
             }
         }
     }
